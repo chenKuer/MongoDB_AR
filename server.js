237215d436dc770d6a1251a58916dc2fcf43808d
chenKuer <chenfjk@gmail.com>
2018-04-14 11:31:48 -0400

    port

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+var port = process.env.PORT || 3000;
 var app = express();
 // const router = express.Router();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -135,5 +137,5 @@ app.get('/flags/:id', async (req, res) => {
     }
 });
 
-app.listen(3000);
+app.set('port', process.env.PORT || 8080);
 module.exports = Flags;
